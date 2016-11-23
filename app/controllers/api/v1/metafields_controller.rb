a4b90f34b7acfeefc14311b0245b5fb23bb8f1b6
Anshul Kothari <akothari@synapseco.com>
2016-11-23 12:48:14 +0530

    commit index

--- app/controllers/api/v1/metafields_controller.rb
+++ app/controllers/api/v1/metafields_controller.rb
@@ -1,19 +1,13 @@
 class API::V1::MetafieldsController < ApplicationController
-	def create
+	def index
 		shop_url = "https://71ce81d53393533f59abaf2c241ab2b6:c75f792a56754bd4e6bfde1174874e48@napura.myshopify.com/admin"
 	    ShopifyAPI::Base.site = shop_url
 	    shop = ShopifyAPI::Shop.current
 
 
-	    @product_list = ShopifyAPI::Metafield.all(params: {resource: 'products', resource_id: metafield_params, fields: 'value'})
+	    @product_list = ShopifyAPI::Metafield.all(params: {resource: 'products', resource_id: 6721712071, fields: 'value'})
 	    respond_to do |format|
       		format.json { render :json => @product_list }
     	end	
 	 end
-
-	private
-
-	def metafield_params
-		params.require(:product).permit(:id)
-	end
 end
